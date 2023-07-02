51301728d2c4d848750dcdfe7aebe673213634cb
Akshat-Borda <bordaakshat007@gmail.com>
2023-07-03 00:58:20 +0530

    done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,30 +9,37 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  let y;
   var t=[];
   var c={};
     
     for(let i=0;i<transactions.length;i++){
       t=transactions[i];
-      if(c[t.catagory]){
-          c[t.catagory]+=t.price;
+      if(c[t.category]){
+          c[t.category]+=t.price;
       
         }else{
-          c[t.catagory]=t.price;
+          c[t.category]=t.price;
           
         }
     }
-  return c;
+    //console.log("hi");
+    var keys=Object.keys(c);
+    var result=[];
+    for(var i=0;i<keys.length;i++){
+      result.push({ category:keys[i] , totalSpent:c[keys[i]]});
+          // console.log(keys[i]);
+          // console.log(c[keys[i]]);
+    }
+  return result;
 }
-let x =[
-  {itemname:'cap', catagory:'clothes', price: 200, timestamp: 12343},
-  {itemname:'shoes', catagory:'clothes', price: 500, timestamp: 2343},
-  {itemname:'bat', catagory:'sports', price: 300, timestamp: 1243},
-  {itemname:'chips', catagory:'food', price: 50, timestamp: 1234},
-  {itemname:'soda', catagory:'food', price: 20, timestamp: 1343},
-  {itemname:'ball', catagory:'sports', price: 80, timestamp: 1534}
-];
-console.log(calculateTotalSpentByCategory(x));
+// let x =[
+//   {itemname:'cap', category:'clothes', price: 200, timestamp: 12343},
+//   {itemname:'shoes', category:'clothes', price: 500, timestamp: 2343},
+//   {itemname:'bat', category:'sports', price: 300, timestamp: 1243},
+//   {itemname:'chips', category:'food', price: 50, timestamp: 1234},
+//   {itemname:'soda', category:'food', price: 20, timestamp: 1343},
+//   {itemname:'ball', category:'sports', price: 80, timestamp: 1534}
+// ];
+// console.log(calculateTotalSpentByCategory(x));
 //calculateTotalSpentByCategory(x);
 module.exports = calculateTotalSpentByCategory;
